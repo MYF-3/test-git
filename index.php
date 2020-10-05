c01353b62c6a6f2537078ca27a311df10c9e5e6f
MuhammadYusufFirstra <yusuf.firstra@gmail.com>
2020-10-05 10:10:23 +0700

    menambahkan paragraf

--- index.php
+++ index.php
@@ -7,5 +7,6 @@
 </head>
 <body>
     <h1> Hello World</h1>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laudantium, quaerat perspiciatis porro ipsam labore odit voluptatum vitae consequuntur unde obcaecati officia odio? Cupiditate obcaecati id sit sequi dicta omnis dolore?</p>
 </body>
 </html>
