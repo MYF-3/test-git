6ba7db97c7f08d8d526c1d7f1266df0c45bc7b5e
MuhammadYusufFirstra <yusuf.firstra@gmail.com>
2020-10-05 10:26:29 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -8,5 +8,9 @@
 <body>
     <h1> Hello World</h1>
     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laudantium, quaerat perspiciatis porro ipsam labore odit voluptatum vitae consequuntur unde obcaecati officia odio? Cupiditate obcaecati id sit sequi dicta omnis dolore?</p>
+    <ul>
+        <li>Satu</li>
+        <li>Dua</li>
+    </ul>
 </body>
 </html>
